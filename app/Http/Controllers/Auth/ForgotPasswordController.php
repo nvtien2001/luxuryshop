65886e3d333e4cab5c8f9091aea67cd7b09e6723
nvtien2001 <nvtiendzl@gmail.com>
2022-12-06 20:20:49 +0700

    manh toan upload

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 class ForgotPasswordController extends Controller
 {
     /*
-    |--------------------------------------------------------------------------
+    |---------------------------------------------------------------------------
     | Password Reset Controller
     |--------------------------------------------------------------------------
     |
